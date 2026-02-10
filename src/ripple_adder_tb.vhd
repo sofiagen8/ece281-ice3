6ee5d064c4efc0702c997e6f58db7d3dfa0be2cf
sofiagen8 <c26sofia.gentile@afacademy.af.edu>
2026-02-10 11:19:16 -0700

    some test benches

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,7 +49,19 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
+	   w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with zeros" severity failure;
+       w_addends <= x"DB"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"9" and w_Cout = '1') report "bad with middle" severity failure;
+       w_addends <= x"41"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"5" and w_Cout = '0') report "bad with middle" severity failure;
+       w_addends <= x"E4"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"2" and w_Cout = '1') report "bad with middle" severity failure;
+       w_addends <= x"85"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"D" and w_Cout = '0') report "bad with middle" severity failure;
+       w_addends <= x"CC"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"9" and w_Cout = '1') report "bad with middle" severity failure;
+       --might need more? not sure how many random ones to add
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
